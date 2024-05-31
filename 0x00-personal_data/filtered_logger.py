1d702975575a76397c080d2a1a8b26f375b69648
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-05-31 16:45:01 +0300

    feat: log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,26 @@
 """filtered_logger."""
 from typing import List
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: str):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        # https://stackoverflow.com/questions/16757578/
+        # what-is-pythons-default-logging-formatter
+        return filter_datum(
+            self.fields, self.REDACTION, super().format(record), self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
