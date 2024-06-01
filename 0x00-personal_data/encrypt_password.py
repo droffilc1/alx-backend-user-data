f72b2d713af4406e33566d0f90a2c5c299539ff8
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-06-01 17:05:59 +0300

    fix: requirement fail

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,13 +5,6 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     """Returns a salted, hashed password."""
-    # Convert the password to bytes
-    password_bytes = password.encode('utf-8')
-
-    # Generate a salt
-    salt = bcrypt.gensalt()
-
-    # Hash the password with the salt
-    hashed_password = bcrypt.hashpw(password_bytes, salt)
-
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'),
+                                    bcrypt.gensalt())
     return hashed_password
