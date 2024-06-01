d946367cbca4e811091b058c4e9e6e20b55bb6df
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-06-01 17:10:34 +0300

    feat: check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,4 +1,4 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 """encrypt_password."""
 import bcrypt
 
@@ -8,3 +8,9 @@ def hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode('utf-8'),
                                     bcrypt.gensalt())
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validates that the provided password matches the hashed password"""
+
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
