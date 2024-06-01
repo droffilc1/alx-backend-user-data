6dbbeb842c6afbdc08e97f8b39536a183c889b39
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-06-01 17:00:07 +0300

    feat: encrypting passwords

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,17 @@
+# usr/bin/python3
+"""encrypt_password."""
+import bcrypt
+
+
+def hash_password(password: str) -> str:
+    """Returns a salted, hashed password."""
+    # Convert the password to bytes
+    password_bytes = password.encode()
+
+    # Generate a salt
+    salt = bcrypt.gensalt()
+
+    # Hash the password with the salt
+    hashed_password = bcrypt.hashpw(password_bytes, salt)
+
+    return hashed_password
