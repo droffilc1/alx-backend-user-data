d05a8c58939e1acdd7d876a9a501667fd7b7a93f
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-06-06 11:05:15 +0300

    feat: create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,10 +2,23 @@
 """session_auth
 Implements SessionAuth class.
 """
+from uuid import uuid4
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """ A class SessionAuth that inherits from Auth
+
+    Attributes:
+        - user_id_by_session_id
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Creates a Session ID for a user_id
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        id = str(uuid4())
+        self.user_id_by_session_id[id] = user_id
+        return id
