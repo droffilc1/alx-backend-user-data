067584bfee906606de46cd45c38013a582a350d0
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-06-06 12:17:04 +0300

    feat: before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -35,14 +35,15 @@ def before_request():
     """
     if auth is None:
         return
-    paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/',
+             '/api/v1/auth_session/login/']
 
     if request.path not in paths and auth.require_auth(request.path, paths):
-        if auth.authorization_header(request) is None:
+        if auth.authorization_header(request) is None and\
+                auth.session_cookie(request) is None:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
-    request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(404)
