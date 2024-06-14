54811a956236620867c0df171cb853dba2d1fcfe
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-06-14 09:42:10 +0300

    fix: find user by session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -85,9 +85,9 @@ class Auth:
             return None
         try:
             user = self._db.find_user_by(session_id=session_id)
+            return user
         except NoResultFound:
             return None
-        return user
 
     def destroy_session(self, user_id: int) -> None:
         """Destroys session
